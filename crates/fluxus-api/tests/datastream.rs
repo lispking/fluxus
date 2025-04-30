220787869659ebd00c8285c6c452f7dd733b9ec3
liyiheng <liyihenggnehiyil@126.com>
2025-04-30 12:40:03 +0800

    feat: limit operator for DataStream

---
+++ crates/fluxus-api/tests/datastream.rs
@@ -0,0 +1,20 @@
+use fluxus_api::{CollectionSink, CollectionSource, DataStream};
+
+#[test]
+fn test_limit() {
+    tokio_test::block_on(async {
+        let numbers = vec![1, 2, 3, 4, 5];
+        // TransformSourceWithOperator::new();
+        let source = CollectionSource::new(numbers);
+        let sink = CollectionSink::new();
+
+        DataStream::new(source)
+            .limit(2)
+            .sink(sink.clone())
+            .await
+            .unwrap();
+
+        let data = sink.get_data();
+        assert_eq!(data, vec![1, 2]);
+    })
+}
